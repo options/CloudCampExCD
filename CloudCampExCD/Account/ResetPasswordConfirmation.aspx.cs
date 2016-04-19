85ac92eb474a695ea06f703a09173137317edeb9
Myung Shin Kim <himskim@msn.com>
2016-04-19 13:39:59 +0900

    initial commit

---
+++ CloudCampExCD/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CloudCampExCD.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CloudCampExCD/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudCampExCD.Startup))]
+namespace CloudCampExCD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
